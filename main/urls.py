5ec6bd138e58395fed42adab164c1f470f2ffb40
Namit Kumar <namit2k1@gmail.com>
2020-05-23 13:37:33 +0530

    temporary check

--- main/urls.py
+++ main/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
   path("register", views.register, name="register"),
   path("logout", views.logout_request, name="logout"),
   path("login", views.login_request, name="login"),
-  path("<single_slug>", views.single_slug, name="single_slug")
+  path("<single_slug>", views.single_slug, name="single_slug"),
+  path('admin/', admin.site.urls),
 ]
