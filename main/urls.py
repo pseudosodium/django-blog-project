3a734cfcea95ebc95cd510761417c0aa3deab5e7
Namit Kumar <namit2k1@gmail.com>
2020-05-23 13:40:00 +0530

    reverted

--- main/urls.py
+++ main/urls.py
@@ -9,6 +9,5 @@ urlpatterns = [
   path("register", views.register, name="register"),
   path("logout", views.logout_request, name="logout"),
   path("login", views.login_request, name="login"),
-  path("<single_slug>", views.single_slug, name="single_slug"),
-  path('admin/', admin.site.urls),
+  path("<single_slug>", views.single_slug, name="single_slug")
 ]
