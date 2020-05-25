20859172f8a3ab87b9ff3d513e577b5c24885c10
Namit Kumar <namit2k1@gmail.com>
2020-05-25 21:34:47 +0530

    Added first_name and last_name fields in user form with changes accordingly

--- main/forms.py
+++ main/forms.py
@@ -3,14 +3,18 @@ from django.contrib.auth.forms import UserCreationForm
 from django import forms
 
 class NewUserForm(UserCreationForm):
+    first_name = forms.CharField(max_length=50, required=True)
+    last_name = forms.CharField(max_length=50)
     email = forms.EmailField(required=True)
 
     class Meta:
         model = User
-        fields = ("username", "email", "password1", "password2")
+        fields = ("username", "first_name", "last_name", "email", "password1", "password2")
 
     def save(self, commit=True):
         user = super(NewUserForm, self).save(commit=False)
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
         user.email = self.cleaned_data['email']
         if commit:
             user.save()
